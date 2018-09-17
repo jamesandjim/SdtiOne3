31bd7fdd1b913a8bc65e25c8b3d0c30278d73a63
james <12348978@qq.com>
2018-09-17 17:47:32 +0800

    20180917

--- apps/deviceManage/views.py
+++ apps/deviceManage/views.py
@@ -10,13 +10,20 @@ def open_door(request):
     dev = WGPaketShort('192.168.0.99', 123300755, 0x40)
     # 定义门号
     dev.udp_data[8] = 1
-    ret = dev.send_data()
+    dev.send_data()
+    ret = dev.rec_data
+    if ret[8] == 1:
+        result = '开门成功'
+    else:
+        result = '开门失败'
 
-    return render(request, 't1.html', {'x': ret})
+    return render(request, 't1.html', {'x': result})
 
 #
 def show_devinfo(request):
     dev = WGPaketShort('192.168.0.99', 123300755, 0x20)
-    ret = dev.send_data()
+    dev.send_data()
+    ret = dev.rec_data
+    
 
     return render(request, 't1.html', {'x': ret})
