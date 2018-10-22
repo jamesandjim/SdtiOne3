e7b40769856cda314918eb79eb639389d60d1d4b
james <12348978@qq.com>
2018-10-22 18:15:25 +0800

    2018102202

--- static/viewmodel/dev_search.js
+++ static/viewmodel/dev_search.js
@@ -1,7 +1,23 @@
 $(function () {
-    $("button#add").click(function () {
-        
+    $("button#add").click(function(){
+		// 遍历所有的table数据
+		var html= new Array();
+		$('table#t1').find('tbody').each(function () {
 
-    })
+            $(this).find('tr').each(function () {
+
+            	$(this).find('td').filter("[class !='td-manage']").each(function () {
+            		html.push($(this).text());
+            	});
+
+
+            });
+
+
+            return false;
+        });
+       alert(html);
+	});
+
 
 })
