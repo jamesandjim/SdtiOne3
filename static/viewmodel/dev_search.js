a149111ef9a40ff0a834d3bac036a76e2116fd9d
james <12348978@qq.com>
2018-10-23 13:55:45 +0800

    2018102301

--- static/viewmodel/dev_search.js
+++ static/viewmodel/dev_search.js
@@ -2,11 +2,20 @@ $(function () {
     $("button#add").click(function(){
 		// 遍历所有的table数据
 		var html= new Array();
+		var title= new Array();
+		title[0]="sn";
+        title[1]="ip";
+        title[2]="netmask";
+        title[3]="netgate";
+        title[4]="mac";
+
+
+
 		$('table#t1').find('tbody').each(function () {
 
             $(this).find('tr').each(function () {
 
-            	$(this).find('td').filter("[class !='td-manage']").each(function () {
+            	$(this).find('td').filter("[class !='td-manage']").filter("[id != 'one']").each(function () {
             		html.push($(this).text());
             	});
 
@@ -16,7 +25,13 @@ $(function () {
 
             return false;
         });
-       alert(html);
+       alert(html[0]);
+       alert(html[1]);
+       alert(html[2]);
+
+
+       //做ajax请求
+
 	});
 
 
